ebcec1e6a5e06e166aabede8dfb9340665127bbf
catalpa65 <catalpa65@163.com>
2025-08-25 15:37:40 +0800

    chapter 9-11

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,6 @@
+/* This is the loading component for the dashboard page. It is used to show a loading state while the dashboard is loading. */
+import DashboardSkeleton from "@/app/ui/skeletons";
+
+export default function Loading() {
+  return <DashboardSkeleton />;
+}
